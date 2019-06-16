764acfb36c38efa17d750df9c7d36e1a135a357f
TrollTown <williamyinanshen2009@gmail.com>
2019-06-16 14:58:02 +1000

    Added javadoc comments to TimePeriod.java

--- src/unsw/venues/TimePeriod.java
+++ src/unsw/venues/TimePeriod.java
@@ -7,20 +7,38 @@ public class TimePeriod implements Comparable<TimePeriod> {
 	private LocalDate startDate;
 	private LocalDate endDate;
 	
+	/**
+	 * Constructor for TimePeriod object: 3 arguments
+	 * @param reservationID Reservation ID
+	 * @param startDate Start date of reservation
+	 * @param endDate End date of reservation
+	 */
 	public TimePeriod(String reservationID, LocalDate startDate, LocalDate endDate) {
 		this.reservationID = reservationID;
 		this.startDate = startDate;
 		this.endDate = endDate;
 	}
 	
+	/**
+	 * This method returns the reservation ID of the TimePeriod object
+	 * @return reservation ID of the TimePeriod object
+	 */
 	public String getReservationID() {
 		return this.reservationID;
 	}
 	
+	/**
+	 * This method returns the start date of the TimePeriod object
+	 * @return start date of the TimePeriod object
+	 */
 	public LocalDate getStartDate() {
 		return this.startDate;
 	}
 	
+	/**
+	 * This method returns the end date of the TimePeriod object
+	 * @return end date of the TimePeriod object
+	 */
 	public LocalDate getEndDate() {
 		return this.endDate;
 	}
