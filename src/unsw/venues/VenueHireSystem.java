ec52a3745aecb32712e100de8a03ea62ee021243
TrollTown <williamyinanshen2009@gmail.com>
2019-06-16 17:52:43 +1000

    Added javadoc comments to the VenueHireSystem.java file and also changed some methods from public to private

--- src/unsw/venues/VenueHireSystem.java
+++ src/unsw/venues/VenueHireSystem.java
@@ -32,7 +32,12 @@ public class VenueHireSystem {
         this.reservations = new ArrayList<Reservation>();
         this.venues = new ArrayList<Venue>();
     }
-
+    
+    /**
+     * Private function that takes in a JSONObject containing a command and executes it by calling
+     * the corresponding function in the switch/case statement
+     * @param json JSONObject in the command format
+     */
     private void processCommand(JSONObject json) {
         switch (json.getString("command")) {
 
@@ -87,7 +92,17 @@ public class VenueHireSystem {
         }
     }
 
-	public JSONObject change(String id1, LocalDate start1, LocalDate end1, int small1, int medium1, int large1) {
+    /**
+     * This method processes any change requests handled by the command parser
+     * @param id1 Reservation ID of the reservation to be changed
+     * @param start1 New start date of the reservation
+     * @param end1 New end date of the reservation
+     * @param small1 New number of small rooms requested
+     * @param medium1 New number of medium rooms requested
+     * @param large1 New number of large rooms requested
+     * @return A JSONObject detailing the new reservation OR { "status" : "rejected" } if failed
+     */
+	private JSONObject change(String id1, LocalDate start1, LocalDate end1, int small1, int medium1, int large1) {
 		JSONObject result = new JSONObject();
         JSONArray rooms = new JSONArray();
         boolean allocationSuccessful = false;
@@ -127,7 +142,13 @@ public class VenueHireSystem {
 		return result;
 	}
 	
-	public JSONObject cancel(String id2) {
+	/**
+	 * This method processes any cancellation requests handled by the command parser
+	 * @param id2 Reservation ID of the reservation to be cancelled
+	 * @return JSONObject with status either success or rejected depending on
+	 * whether or not the reservation could be found on the system
+	 */
+	private JSONObject cancel(String id2) {
 		JSONObject result = new JSONObject();
 		Reservation reservation = getReservationByID(id2);
 		if (reservation != null) {
@@ -144,7 +165,12 @@ public class VenueHireSystem {
 		return result;
 	}
 	
-	public JSONArray list(String venue) {
+	/**
+	 * This method processes any list requests handled by the command parser
+	 * @param venue The venue which should have its reservation information displayed
+	 * @return A JSONArray of objects which detail the reservation information for each room in the venue
+	 */
+	private JSONArray list(String venue) {
 		JSONArray result = new JSONArray();
 		Venue venueObj = getVenueByName(venue);
 		for (int i = 0; i < venueObj.getRooms().size(); i++) {
@@ -170,6 +196,13 @@ public class VenueHireSystem {
 		return result;
 	}
 	
+	/**
+	 * This is a private method that calls the addRoom method of the relevant venue so that a room is added.
+	 * If the venue does not exist, it is first created and then the room is added.
+	 * @param venue Name of the venue to which the room is to be added to
+	 * @param roomName Name of the room to be added
+	 * @param size Size of the room
+	 */
 	private void addRoom(String venue, String roomName, String size) {
 		// If venue doesn't already exist add it and then add the room
 		if (getVenueByName(venue) == null) {
@@ -177,8 +210,19 @@ public class VenueHireSystem {
         }	
 	    getVenueByName(venue).addRoom(roomName, size);
     }
-
-    public JSONObject request(String id, LocalDate start, LocalDate end,
+	
+	/**
+	 * This is a private method that requests a room by calling the requestRooms method on venues in the order
+	 * they were entered into the system.
+	 * @param id ID of the reservation
+	 * @param start	Start date of the reservation
+	 * @param end End date of the reservation
+	 * @param small Number of small rooms requested
+	 * @param medium Number of medium rooms requested
+	 * @param large Number of large rooms requested
+	 * @return Returns a JSONObject detailing the reservation if successful or status: rejected if unsuccessful
+	 */
+    private JSONObject request(String id, LocalDate start, LocalDate end,
             int small, int medium, int large) {
     	JSONObject result = new JSONObject();
         JSONArray rooms = new JSONArray();
@@ -216,7 +260,13 @@ public class VenueHireSystem {
         }
         return result;
     }
-
+    
+    /**
+     * This is the main function of the venue hire system that takes in input
+     * and turns each line into a JSONObject which is then passed to the
+     * processCommand function to be executed
+     * @param args
+     */
     public static void main(String[] args) {
         VenueHireSystem system = new VenueHireSystem();
 
@@ -235,6 +285,12 @@ public class VenueHireSystem {
         sc.close();
     }
     
+    /**
+     * This method returns the relevant reservation object given its ID
+     * @param id ID of the reservation to be retrieved
+     * @return Reservation object with the corresponding ID or null
+     * if the ID could not be found on the system
+     */
     public Reservation getReservationByID(String id){
     	for (int i = 0; i < this.reservations.size(); i++) {
     		if (this.reservations.get(i).getID().equals(id)) {
@@ -244,15 +300,29 @@ public class VenueHireSystem {
     	return null;
     }
     
+    /**
+     * This method returns the list of venues stored within the system
+     * @return ArrayList of all venues stored in the system
+     */
     public ArrayList<Venue> getVenues(){
     	return this.venues;
     }
     
+    /**
+     * This private method adds a venue to the system 
+     * (called by the addRoom function if a venue cannot be found)
+     * @param name Name of the venue to be added
+     */
     private void addVenue(String name) {
     	Venue newVenue = new Venue(name);
     	this.venues.add(newVenue);
     }
     
+    /**
+     * This private method returns the relevant venue object given its name
+     * @param name Name of the venue
+     * @return Venue object with the corresponding name
+     */
     private Venue getVenueByName(String name) {
     	for (int i = 0; i < this.venues.size(); i++) {
     		if (this.venues.get(i).getName().equals(name)) {
